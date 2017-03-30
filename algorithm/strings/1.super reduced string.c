95aff3e734df188c55bc4ff73e6bb19e9a3a9430
Purva Singh <singhpurva2906@gmail.com>
2017-03-30 20:04:15 +0530

    Create 1.super reduced string.c

---
+++ algorithm/strings/1.super reduced string.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+	char str[100];
+	scanf("%s", &str);
+	char *stack = (char *)malloc(sizeof((char* )strlen(str)));
+	int top=-1, i;
+	for(i=0;i<strlen(str);i++){
+		if(i==0){
+			stack[++top]=str[i];
+		}
+		else{
+			if(stack[top]==str[i]){
+				top--;
+			}
+			else{
+				stack[++top]=str[i];
+			}
+		}
+	}
+	if(top==-1){
+		printf("Empty String");
+	}
+	else{
+		for(i=0;i<=top;i++){
+			printf("%c", stack[i]);
+		}
+	}
+	return 0;
+}
