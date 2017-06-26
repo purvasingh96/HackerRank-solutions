55cb2b683869cb4604c9ef2d97f4130b9354c91b
Purva Singh <singhpurva2906@gmail.com>
2017-06-26 18:12:36 +0530

    Create day 27.cpp

---
+++ 30 days of code/day 27.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"5"<<endl; 
+    cout<<"5 3"<<endl; cout<<"-1 90 999 100 0"<<endl;
+    cout<<"4 2"<<endl; cout<<"0 -1 2 1"<<endl;
+    cout<<"3 3"<<endl; cout<<"-1 0 1"<<endl;
+    cout<<"6 1"<<endl; cout<<"-1 0 1 -1 2 3"<<endl;
+    cout<<"7 3"<<endl; cout<<"-1 0 1 2 3 4 5"<<endl;   
+}
