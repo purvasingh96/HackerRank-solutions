0ef363be632b2cc0362e7777f29156f21b386476
Purva Singh <singhpurva2906@gmail.com>
2017-06-26 18:05:02 +0530

    Create day 16.cpp

---
+++ 30 days of code/day 16.cpp
@@ -0,0 +1,39 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    string S;
+    cin >> S;
+    try{
+        string::size_type sz;
+        int x = stoi(S, &sz);
+        cout<<x;
+    }
+    catch(...){
+        cout<<"Bad String";
+    }
+    return 0;
+}
