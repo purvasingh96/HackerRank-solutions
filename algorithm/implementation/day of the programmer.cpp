e92a29f1869647f465d516106f3adb9eff6f83a4
Purva Singh <singhpurva2906@gmail.com>
2017-07-21 16:11:08 +0530

    Create day of the programmer.cpp

---
+++ algorithm/implementation/day of the programmer.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include<string.h>
+using namespace std;
+
+string solve(int year){
+    // Complete this function
+    //int sum=215, diff;
+    string result;
+    if(year>=1700 && year<=1917)
+    {
+    	cout<<"julian calender";
+    	if(year%4==0)
+    	{
+    		cout<<"julian and leap year";
+    		result = "12.09."+to_string(3.1415926);
+		}
+		else{
+			cout<<"julian and not leap";
+			result = "13.09."+year;
+		}
+	}
+	else if(year==1918)
+	{
+		return "26.09.1918";
+	}
+	else 
+	{
+		if(year%400==0 && year%4==0 && year%100!=0)
+		{
+			cout<<"georgian and leap";
+			result = "12.09."+year;
+		}
+		else {
+			cout<<"georgian and nor leap";
+			result = "13.09."+year;
+		}
+	}
+	return result;
+}
+
+int main() {
+    int year;
+    cin >> year;
+    string result = solve(year);
+    cout << result << endl;
+    return 0;
+}
