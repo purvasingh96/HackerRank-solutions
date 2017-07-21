1c6f69041d9280a84c5ded01153d9b6ad760884b
Purva Singh <singhpurva2906@gmail.com>
2017-07-21 16:11:58 +0530

    Update day of the programmer.cpp

--- algorithm/implementation/day of the programmer.cpp
+++ algorithm/implementation/day of the programmer.cpp
@@ -8,15 +8,15 @@ string solve(int year){
     string result;
     if(year>=1700 && year<=1917)
     {
-    	cout<<"julian calender";
+    	//cout<<"julian calender";
     	if(year%4==0)
     	{
-    		cout<<"julian and leap year";
-    		result = "12.09."+to_string(3.1415926);
+    		//cout<<"julian and leap year";
+    		result = "12.09."+to_string(year);
 		}
 		else{
-			cout<<"julian and not leap";
-			result = "13.09."+year;
+			//cout<<"julian and not leap";
+			result = "13.09."+to_string(year);
 		}
 	}
 	else if(year==1918)
@@ -25,14 +25,14 @@ string solve(int year){
 	}
 	else 
 	{
-		if(year%400==0 && year%4==0 && year%100!=0)
+		if(year%400==0 || (year%4==0 && year%100!=0))
 		{
-			cout<<"georgian and leap";
-			result = "12.09."+year;
+			//cout<<"georgian and leap";
+			result = "12.09."+to_string(year);
 		}
 		else {
-			cout<<"georgian and nor leap";
-			result = "13.09."+year;
+			//cout<<"georgian and nor leap";
+			result = "13.09."+to_string(year);
 		}
 	}
 	return result;
