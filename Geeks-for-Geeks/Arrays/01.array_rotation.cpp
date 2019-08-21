4eaba284de10605963ad790991f4bf2c5adab566
Purva Singh <singhpurva2906@gmail.com>
2019-08-21 09:11:38 +0530

    rotate and delete

--- Geeks-for-Geeks/Arrays/01.array_rotation.cpp
+++ Geeks-for-Geeks/Arrays/01.array_rotation.cpp
@@ -1,1 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector <long long int> v;
+    long long int i, n, t,te;
+    cin>>te;
+    while(te>0)
+    {
+        cin>>n;
+        for(i=0; i<n; i++)
+        {
+            cin>>t;
+            v.push_back(t);
+        }
+        i=1;
+        while(i<(n/2)+1)
+        {
+            rotate(v.begin(),v.end()-1,v.end());
+            v.erase (v.begin()+(v.size()-i));
+            i++;
+        }
+        cout<<v[0]<<endl;
+        --te;
+        v.clear();
+    }
+return 0;
+}
 
